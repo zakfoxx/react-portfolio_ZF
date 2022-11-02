a365858af0dc1807490fbaf0d9067fba6f80479e
Zechariah Foxx <zak.foxx@gmail.com>
2022-11-01 21:04:59 -0400

    style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./style.css";
+
 export default function Header() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/aboutme">About Me</Link>
-        </li>
-      </ul>
+    <div id="header">
+      <h1> Zechariah Foxx</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/aboutme">About Me</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
